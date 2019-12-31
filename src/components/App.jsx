ecdcfeec56e290fd8fd247cd37caa209f0650fdc
psabella <pamsabella@gmail.com>
2019-12-30 17:36:17 -0800

    render partial video list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,3 +26,5 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,15 +1,10 @@
-import defaultExport from '/src/data/exampleVideoData.js';
-var VideoList = (exampleVideoData) => (
+import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+var VideoList = (props) => (
   <div className="video-list">
-    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>*/
-      <div><h5><em>videoListEntry</em> </h5></div>
-      // exampleVideoData.map(entry =>
-      //   <VideoListEntry entry={entry} />
-      //)
-    }
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
 
